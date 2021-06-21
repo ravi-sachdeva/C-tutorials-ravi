f2fa63d52db241d6798eac1f41c12df30d60922e
Ravi Sachdeva <ravisachdeva224235@gmail.com>
2021-06-21 22:26:28 +0530

    video 03

---
+++ lecture03.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int age,marks,rollno;
+	
+	// ( >> Extraction Operator ) Cin - takes input from keyboard
+	// ( << Insertion Operator ) Cout  gives output to monitor / console
+	
+	
+	string name;
+	
+	cout<<"Enter the age, marks  roll no and name: ";
+	cin>>age>>marks>>rollno>>name;
+	cout<<"Age = "<<age<<endl;
+	cout<<"Marks = "<<marks<<endl;
+	cout<<"Roll No = "<<rollno<<endl;
+	cout<<"Name = "<<name<<endl; 
+
+//	string colgname;
+//	getline(cin,colgname);
+//	cout<<"My colg name is : "<<colgname;
+
+
+	    
+}                         
+	
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<<"Hello World";
+	cin>>a;
+	return 0;
+}
